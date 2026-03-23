10b73ac44dfb96d240e818adda397a467b87e96f
Alberto Monterroso <14013679+Albermonte@users.noreply.github.com>
2026-03-23 22:27:55 +0100

    fix(wallet): add qrcode type declaration

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,3 +5,16 @@ declare module "*.vue" {
   const component: DefineComponent<Record<string, never>, Record<string, never>, unknown>;
   export default component;
 }
+
+declare module "qrcode" {
+  interface QRCodeToDataUrlOptions {
+    margin?: number;
+    width?: number;
+    color?: {
+      dark?: string;
+      light?: string;
+    };
+  }
+
+  export function toDataURL(text: string, options?: QRCodeToDataUrlOptions): Promise<string>;
+}
